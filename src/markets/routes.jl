c765a8d640d06124ac30d0d292867ff282dcd8c9
Steve Kuhlman <spkuhlman@outlook.com>
2022-06-09 08:59:23 -0400

    Dropping db's adding web

--- src/markets/routes.jl
+++ src/markets/routes.jl
@@ -1,5 +1,18 @@
-using Genie.Router
+using Genie, Genie.Router, Genie.Renderer.Json, Genie.Requests
 
+
+conn=RedisConnection()
 route("/") do
   serve_static_file("welcome.html")
+end
+
+route("/track", method = POST) do 
+  parseTracking(postpayload())   
+end
+
+function parseTracking(payload)
+#  println("Payload: $payload")
+#  println("Labels: $(payload[:JSON_PAYLOAD])")
+  labels=payload[:JSON_PAYLOAD]["labels"]
+  println("Labels: $(labels) ")
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,11 @@
 using JPPF
-using Test
+using Test, HTTP, JSON
+#Assumes the server is started first
+
+url="http://localhost:8000/track"
+trackJSON="""{"labels":["INT", "JPM", "IBM"]}"""
 
 @testset "JPPF.jl" begin
-    # Write your tests here.
+  resp=HTTP.request("POST", url, [("Content-Type", "application/json")], trackJSON)
+  @test resp.status == 200
 end
