56c7b418553bdccdb28631bb03287a18a2f5e7b7
Steve Kuhlman <spkuhlman@outlook.com>
2022-07-05 15:59:31 -0400

    debugged replicate holdings

--- src/updateHoldings.jl
+++ src/updateHoldings.jl
@@ -17,7 +17,7 @@ findHolding(holdings, symbol)=findnext(h -> h.stock.symbol == symbol, holdings,1
 "Update a holding"
 function updateHolding!(h, changes) 
   thisChange=findHolding(changes, h.stock.symbol)
-  h.stock.count+=thisChange.stock.chount
+  h.stock.count+=thisChange.stock.count
 end
 
 """Updates holdings to totals by day
@@ -36,12 +36,14 @@ function fillOutHoldings!(holdings, holdingsChanges, fromDate::Date, toDate::Dat
     todayChanges = filter(x -> x.date == day, holdingsChanges)#Changes today 
     existingChanges=∩(todayChanges, todayHoldings) #Set of existing holdings with changes today
     newChanges=setdiff(todayChanges, todayHoldings) #Set of new holdings with changes today
+    todayNotChanged=setdiff(todayHoldings, todayChanges)
     todayPlusExisting=map(h -> updateHoldings(h, todayChanges), existingChanges)
-    holdings=vcat(holdings, todayChanges, newChanges, todayPlusExisting)
+    holdings=vcat(holdings, newChanges, todayPlusExisting, todayNotChanged)
   end
   holdings
 end
 
+#TODO if holding count == 0 delete
 #Sample data for holdings
 JPM=Stock("JPM", "JP Morgan and Chase")
 INTC=Stock("INTC", "Intel Corporation")
@@ -52,4 +54,5 @@ push!(holdingsChanges, Holding(today()-Day(4), JPM, 20))
 push!(holdingsChanges, Holding(today()-Day(3), INTC, 30))
 push!(holdingsChanges, Holding(today()-Day(2), AMD, 40))
 push!(holdingsChanges, Holding(today()-Day(1), AMD,  50))
-
+push!(holdingsChanges, Holding(today()-Day(1), JPM, -20))
+fillOutHoldings!(holdings, holdingsChanges, today()-Day(5))
