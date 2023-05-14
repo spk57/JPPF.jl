ecc95c5ba85360faf6452a84a43a232001ded523
spk57 <spk57@users.noreply.github.com>
2023-05-13 22:46:45 -0400

    Update Jenkins script with mount point for code

---
+++ src/JPPF_daily.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Script to run from cron for daily updates to market information 
+
+echo "JPPF_daily.sh" 
--- src/buildJenkins.sh
+++ src/buildJenkins.sh
@@ -2,4 +2,9 @@
 # http://localhost:49001/
 mkdir $HOME/jenkins_home
 chown 1000 $HOME/jenkins_home
-docker run -d -p 49001:8080 -v $HOME:/var/jenkins_home/jenkins --name jenkins jenkins/jenkins:jdk11
+#docker run -d -p 49001:8080 -v $HOME:/var/jenkins_home/jenkins --name jenkins jenkins/jenkins:jdk11
+#This works
+#sudo docker run -d -v jenkins_home:/var/jenkins_home -p 8080:8080 -p 50000:50000 --restart=on-failure jenkins/jenkins:lts-jdk11
+sudo docker run -d -v jenkins_home:/var/jenkins_home \
+  --mount type=bind,source=/home/steve/dev/projects/JPPF.jl,target=/src \
+  -p 8080:8080 -p 50000:50000 --restart=on-failure jenkins/jenkins:lts-jdk11
