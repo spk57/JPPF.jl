2acf4196356200ca398a9d5c7aeaf55b04f94ec9
spk57 <spk57@users.noreply.github.com>
2023-06-25 11:27:51 -0400

    Added PersonalFinance.jl

---
+++ src/PersonalFinance.jl
@@ -0,0 +1,123 @@
+"PersonalFinance.jl"
+module PersonalFinance
+using Base: _throw_argerror
+using DataFrames: Dict
+using Markdown, DataFrames, Query, XLSX, Polynomials
+include("RetirementBudget.jl")
+
+export readAssetList, qtrAssetTotals, roundThousands, sumNetAssets, sumAssets, fitAssets, 
+  fitPoints, monthlyGrowth, ht, getCurrentAssets, readTab, pareto
+
+export readRetirementBudget, getScenario, getScenarioElements, filterScenario
+
+assetHistoryTab="AssetHistory";
+qtrTab="Qtrs"; 
+defaultXLS="../data/AssetListApril2021.xlsx"
+currentTab="CurrentAssets"
+
+"Read an excel tab into a dataframe"
+readTab(xls, tabName)=DataFrame(XLSX.readtable(xls, tabName, infer_eltypes=true)...)
+
+"read asset lists from excel file"
+function readAssetList(xls)
+  assetHistory = DataFrame(XLSX.readtable(xls, assetHistoryTab, infer_eltypes=true)...);
+  assetHistory.Value=convert.(Float64, assetHistory.Amount);
+  assetHistory.Savings=convert.(Float64, assetHistory.Savings);
+  assetHistory.Growth=convert.(Float64, assetHistory.Growth);
+  qtrTable=	XLSX.readtable(xls, qtrTab, infer_eltypes=true);
+  quarters = DataFrame(qtrTable...);
+  currentAssets=DataFrame(XLSX.readtable(xls, currentTab, infer_eltypes=true)...);
+  (assetHistory, quarters, currentAssets)
+end
+
+"join list of quarters to data points."
+function qtrAssetTotals(assetHistory, quarters)
+  assetsQtr = @from i in assetHistory begin
+    @join j in quarters on i.YearQtr equals j.YearQtr
+    @select {j.Qtr, j.YearQtr, i.Value, i.Savings, i.Growth }
+    @collect DataFrame
+  end
+  assetsQtr
+end
+
+"Divide by 1000 and round"
+roundThousands(f, digits=1)=round(f/1000.0, digits=digits)
+
+"CurrentAssets"
+function getCurrentAssets(assetHistory, currentAssets, yq)
+  currentAssets= @from i in assetHistory begin
+    @join j in currentAssets on i.AssetID equals j.AssetID
+    @where i.YearQtr == yq
+    @select {j.Name, i.Value, i.Savings, i.Growth }
+    @collect DataFrame
+  end
+  currentAssets
+end
+
+"Sum net assets by quarter"
+function sumNetAssets(assetsQtr)
+  netAssets = assetsQtr |>
+    @groupby(_.Qtr,  (YearQtr=_.YearQtr, Qtr=_.Qtr, Value=_.Value)) |>
+    @map({Qtr=key(_), Value=roundThousands(sum(_.Value)) }) |>
+  	@orderby(_)|> DataFrame
+  netAssets
+end
+
+"Sum assets or liabilities by quarter.  set assets = false for liabilities"
+function sumAssets(assetsQtr, assets=true)
+  netAssets = assetsQtr |>
+    @groupby(_.Qtr, (YearQtr=_.YearQtr, Qtr=_.Qtr, Value=_.Value, Savings=_.Savings, Growth=_.Growth)) |>
+    @map({Qtr=key(_), Net        =roundThousands(sum(    _.Value)), 
+                      Assets     =roundThousands(sum(gtz,_.Value)),
+                      Liabilities=roundThousands(sum(ltz,_.Value)), 
+                      Savings    =roundThousands(sum(    _.Savings)), 
+                      Growth     =roundThousands(sum(    _.Growth))}) |>
+  	@orderby(_)|> DataFrame
+  netAssets
+end
+
+"First order fit for asset growth"
+fitAssets(x, y)=fit(x, y, 1)
+
+"Fit points to a curve"
+fitPoints(curve, last)=[curve[1] + curve[2]*x for x=1:last] 
+
+"fit line to assets"
+function  monthlyGrowth(netAssets, poly)
+  #fit(netAssets.Qtr, netAssets.Value, 1) |> p -> round.(coeffs(p), digits=1) |> Polynomial;
+	monthlyGrowth=round(poly.coeffs[2]/3,digits=1);
+  currNet=round(netAssets.Value[end],digits=1)
+  netGrowth=round(netAssets.Value[end] - netAssets.Value[1], digits=1)
+  nQuarters=netAssets.Qtr[end] - netAssets.Qtr[1]
+  (currNet, monthlyGrowth, netGrowth, nQuarters)
+end
+
+"Display html"
+ht(s)=display("text/html",s )
+hr(s, color)=display("text/html", "<span style=\"color:$color\">$s</span>")
+
+"Greater than zero"
+gtz(x)= x > 0 ? x : 0
+
+"Less Than Zero"
+ltz(x)= x <= 0 ? x : 0
+
+"""
+Pareto filter of a data frame. 
+returns the top elements of the list and the other elements as a tuple
+"""
+function pareto(df, col, rows=10,rounding=true)
+  total=sum(df[:,col])
+  sorted=sort(df, col, rev=true)
+  cols=size(df,2)
+  sortedP=select(sorted, :, col=> cumsum => :Cumsum, col=>(v-> cumsum(v) / total*100)=>:Pct)
+  if rounding
+    sortedP[:,:Pct] = round.(sortedP.Pct, digits=1)
+  end
+  sortedP.Rank=rownumber.(eachrow(sortedP))
+  top=sortedP[1:rows,:]
+  other= sortedP[rows+1:end, :]
+  (top, other)
+end
+
+end
