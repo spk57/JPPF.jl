bb57c586080dcc08cd376028b34987ab1fde82e7
spk57 <spk57@users.noreply.github.com>
2023-08-01 16:41:14 -0400

    added design for webfile layout, code to create web folders

--- src/JPPF.jl
+++ src/JPPF.jl
@@ -12,6 +12,17 @@ const program=@__MODULE__
 
 about()="$program Version: $version $(string(started))"
 
+const htmlStr="~~~"
+"write and flush a string to the io"
+writefl(io, str)=begin write(io, str);flush(io)end
+
+"write an HTML string to the output"
+writeHTML(io, str)=write(io, string(htmlStr, str, htmlStr))
+
+const anaSuffix="_ANA"
+"get a list of analysis folders"
+getAnalysisFolders(dir)= filter(f-> occursin(anaSuffix, f),  readdir(dir))
+
 "Main function to run JPPF"
 function run(dataDir="data", configFile="config.json", port=8001)
   @info about()
@@ -31,7 +42,16 @@ function run(dataDir="data", configFile="config.json", port=8001)
   configTRE=config["RegularExpressions"]
   webPath=joinpath(dataDir, config["webFolder"])
   @info "Web Path: $webPath"
-  t=startWeb(webPath)
+  indexWeb=joinpath(webPath, "index.md")
+  @info "Index.md $indexWeb"
+  anapath=joinpath(webPath, Dates.format(today, "yyyymm")* anaSuffix)
+  isdir(anapath) ? println("[ Note: Analysis Path already exists $anapath") : mkpath(anapath)
+  @info "Analysis Path: $anapath"
+  anaFolders=getAnalysisFolders(webPath)
+  @info "Analysis Folders: $anaFolders"
+#  indexIO=open(indexWeb, "a")
+#  t=startWeb(webPath)
+
 end
 
 end
---
+++ src/webFileLayout.md
@@ -0,0 +1,8 @@
+# JPPF Franklin web file layout 
+
+* Index  Home page with listing of Analysis Folders
+* MonthYear Folders containing web analysis for a given Month/Year
+  *   config.md     Configuration for the analysis 
+  *   config.json   Copy of the config file when the model was run 
+  *   inputData.md  Summary of input data 
+  *   analysis.md   Results of Analysis 
