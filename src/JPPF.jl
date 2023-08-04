e01d033037c768b00f82161fd535f7428e1edfed
spk57 <spk57@users.noreply.github.com>
2023-08-04 11:20:13 -0400

    Debugged initial web page for Franklin

--- src/JPPF.jl
+++ src/JPPF.jl
@@ -1,6 +1,6 @@
 "Julia Programmers Personal Finance"
 module JPPF
-using Dates, Pkg, JSON
+using Dates, Pkg, JSON, Hyperscript
 
 #include("PersonalFinance.jl")
 include("setupFranklin.jl")
@@ -23,8 +23,49 @@ const anaSuffix="_ANA"
 "get a list of analysis folders"
 getAnalysisFolders(dir)= filter(f-> occursin(anaSuffix, f),  readdir(dir))
 
+const indexText = """
+@def title = "Julia Programmers Personal Finance"
+# Julia Programmers Personal Finance
+Personal Finance Tools intended to be flexible for people with Julia Language development skills. 
+## Analysis Folders: 
+"""
+const folderIndex = """
+@def title = "Julia Programmers Personal Finance Folder"
+## Analysis Folder: 
+"""
+
+const sp=" "
+"Parse Dir Name"
+parseDirName(dir)=monthabbr(parse(Int, SubString(dir, 5:6))) * sp * SubString(dir, 1:4) * sp * SubString(dir, 7:9)
+
+"Write the index.md file within a folder"
+writeFolderIndex(path)=write(path, folderIndex)
+
+hTable=m("table")
+tr=m("tr")
+th=m("th")
+td=m("td")
+
+"Build html representation of a header"
+tableHeader(table)=map(th, columnnames(table))
+
+"Build html representation of a row"
+tableRow(table, row)=tr(map(td, values(table[row])))
+
+"Build html representation of all rows"
+tableBody(table)=map(r -> tableRow(table, r), 1:length(table))
+
+"Build html representation of a table"
+tableHTML(table)=hTable(tableHeader(table), tableBody(table))
+
+"Build Markup representation directory list"
+function dirMarkupList(dir) 
+  label=parseDirName(dir)
+  string("* [", label, "](", dir, ")")
+end
+
 "Main function to run JPPF"
-function run(dataDir="data", configFile="config.json", port=8001)
+function run(dataDir="data", configFile="config.json", rVersion="v01", port=8001)
   @info about()
   @info "dataDir: $dataDir"
   @info "configFile: $configFile" 
@@ -44,14 +85,16 @@ function run(dataDir="data", configFile="config.json", port=8001)
   @info "Web Path: $webPath"
   indexWeb=joinpath(webPath, "index.md")
   @info "Index.md $indexWeb"
-  anapath=joinpath(webPath, Dates.format(today, "yyyymm")* anaSuffix)
+  anapath=joinpath(webPath, Dates.format(today, "yyyymm")* rVersion * anaSuffix)
   isdir(anapath) ? println("[ Note: Analysis Path already exists $anapath") : mkpath(anapath)
   @info "Analysis Path: $anapath"
-  anaFolders=getAnalysisFolders(webPath)
-  @info "Analysis Folders: $anaFolders"
-#  indexIO=open(indexWeb, "a")
-#  t=startWeb(webPath)
-
-end
-
-end
+  open(indexWeb, "w") do indexIO
+    write(indexIO, indexText)
+    folders=getAnalysisFolders(webPath)
+    folderList=map(dirMarkupList, folders)
+    @info "Analysis Folders $folderList"
+    map(f -> println(indexIO, f), folderList)
+  end
+  t=startWeb(webPath)
+end #run
+end #module
--- src/webFileLayout.md
+++ src/webFileLayout.md
@@ -1,7 +1,7 @@
 # JPPF Franklin web file layout 
 
 * Index  Home page with listing of Analysis Folders
-* MonthYear Folders containing web analysis for a given Month/Year
+* MonthYear Folders containing web analysis for a given Month/Year.Version
   *   config.md     Configuration for the analysis 
   *   config.json   Copy of the config file when the model was run 
   *   inputData.md  Summary of input data 
