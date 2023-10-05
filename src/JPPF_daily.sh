507128a31948f477c0340c70e6fba1eb723200b2
spk57 <spk57@users.noreply.github.com>
2023-10-05 13:59:58 -0400

    Working issues with script

--- src/JPPF_daily.sh
+++ src/JPPF_daily.sh
@@ -5,14 +5,14 @@ date
 echo "Started JPPF_daily.sh" 
 export PATH=$PATH:/julia/bin
 echo "Path: " $PATH
-export JULIA_DEPOT_PATH="/depot"
-echo "JULIA_DEPOT: " $JULIA_DEPOT_PATH
-export JULIA_PROJECT="/src"
-echo "JULIA_PROJECT: " $JULIA_PROJECT
+#export JULIA_DEPOT_PATH="/depot"
+#echo "JULIA_DEPOT: " $JULIA_DEPOT_PATH
+#export JULIA_PROJECT="/src"
+#echo "JULIA_PROJECT: " $JULIA_PROJECT
 export JPPF_DATA="/home/steve/dev/projects/jppfdata"
 echo "JPPF_DATA: " $JPPF_DATA
 export JPPF_TICK=$JPPF_DATA/track.json
 echo "JPPF_TICK: " $JPPF_TICK
 julia -e 'using Pkg;Pkg.status()'
-julia --project UpdateValues.jl -c JPPF_TICK -o $JPPF_DATA  
+julia --project src/UpdateValues.jl -c $JPPF_TICK -o $JPPF_DATA  
 
