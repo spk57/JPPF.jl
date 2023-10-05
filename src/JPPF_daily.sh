23f361a9ffcb5be65faeef77c92144b787083090
spk57 <spk57@users.noreply.github.com>
2023-10-05 13:33:15 -0400

    Updated JPPF_daily.sh to call UpdateValues.jl

--- src/JPPF_daily.sh
+++ src/JPPF_daily.sh
@@ -3,12 +3,16 @@
 
 date
 echo "Started JPPF_daily.sh" 
-cd src
-echo "cd src: " pwd
 export PATH=$PATH:/julia/bin
 echo "Path: " $PATH
 export JULIA_DEPOT_PATH="/depot"
 echo "JULIA_DEPOT: " $JULIA_DEPOT_PATH
 export JULIA_PROJECT="/src"
 echo "JULIA_PROJECT: " $JULIA_PROJECT
+export JPPF_DATA="/home/steve/dev/projects/jppfdata"
+echo "JPPF_DATA: " $JPPF_DATA
+export JPPF_TICK=$JPPF_DATA/track.json
+echo "JPPF_TICK: " $JPPF_TICK
 julia -e 'using Pkg;Pkg.status()'
+julia --project UpdateValues.jl -c JPPF_TICK -o $JPPF_DATA  
+
