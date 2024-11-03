2938b2c661de6ebcec22b5a730b6cd56706b5b09
spk57 <3463852+spk57@users.noreply.github.com>
2024-11-03 11:12:06 -0500

    separated output from config folders

--- src/JPPF_daily.sh
+++ src/JPPF_daily.sh
@@ -5,13 +5,11 @@ date
 echo "Started JPPF_daily.sh" 
 export PATH=$PATH:/julia/bin
 echo "Path: " $PATH
-#export JULIA_DEPOT_PATH="/depot"
-#echo "JULIA_DEPOT: " $JULIA_DEPOT_PATH
-#export JULIA_PROJECT="/src"
-#echo "JULIA_PROJECT: " $JULIA_PROJECT
+export JPPF_CONFIG=$(realpath ~/data/jppfconfig)
+echo "JPPF_CONFIG: $JPPF_CONFIG" 
 export JPPF_DATA=$(realpath ~/data/jppfdata)
 echo "JPPF_DATA: " $JPPF_DATA
-export JPPF_TICK=$JPPF_DATA/track.json
+export JPPF_TICK=$JPPF_CONFIG/track.json
 echo "JPPF_TICK: " $JPPF_TICK
 #julia -e 'using Pkg;Pkg.status()'
 julia --project src/UpdateValues.jl -c $JPPF_TICK -o $JPPF_DATA  
